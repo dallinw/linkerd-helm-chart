ceeb2e5a08c9e07d5d61371c114ec48f856c93be
Dallin Wright <dallin.wright@supportlogic.io>
2022-11-09 14:00:42 +0100

    working bash script

--- local_cluster_bootstrap.sh
+++ local_cluster_bootstrap.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env zsh
 
-# Prep
+# Small util script to redeploy a whole cluster from scratch. It is for local use and of course not an ideal solution.
+
+# Point at docker-desktop, make sure to not point at any other cluster
+kubectl config use-context docker-desktop
+
+# Prep, delete old data if it exists
+kubectl delete namespace linkerd
+kubectl delete namespace ambassador
+kubectl delete namespace services
+kubectl delete namespace linkerd-viz
+
+# remove temp files, if they exist
 rm -Rf linkerd-control-plane
 rm ca.key
 rm ca.crt
-
-# Point at docker-desktop
-kubectl config use-context docker-desktop
 
 # Ambassador ingress
 helm repo add datawire https://app.getambassador.io
 helm repo update
 
+# Create the namespace, and the remote CRD's have coded inside namespace designators.
 kubectl create namespace ambassador
 kubectl apply -f https://app.getambassador.io/yaml/edge-stack/latest/aes-crds.yaml
 
@@ -23,29 +32,35 @@ datawire/edge-stack \
 --set emissary-ingress.agent.cloudConnectToken=$AMBASSADOR_CLOUD_TOKEN \
 -f ../ambassador-helm-chart/values.yaml
 
-# Cert manager
+# Cert manager, used for intra-service mTLS
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade -i \
-    cert-manager jetstack/cert-manager \
-    --namespace cert-manager \
-    --create-namespace \
-    --version v1.10.0 \
-    --set installCRDs=true
+cert-manager jetstack/cert-manager \
+--namespace cert-manager \
+--create-namespace \
+--version v1.10.0 \
+--set installCRDs=true
 
 # Linkerd service mesh
 helm repo add linkerd https://helm.linkerd.io/stable
 helm repo update
 
+# Install linkerd CRD's
 helm install linkerd-crds linkerd/linkerd-crds -n linkerd --create-namespace
 
+# Create local seed cert CA
 step certificate create root.linkerd.cluster.local ca.crt ca.key \
-    --profile root-ca --no-password --insecure &&
+    --profile root-ca --no-password --insecure
+
 kubectl create secret tls \
 linkerd-trust-anchor \
 --cert=ca.crt \
 --key=ca.key \
 --namespace=linkerd
+
+# Apply the special linkerd to cert manager tie-in
+kubectl apply -f setup/certificates.yaml
 
 helm fetch --untar linkerd/linkerd-control-plane && \
 helm upgrade -i \
@@ -58,13 +73,10 @@ helm upgrade -i \
     -f values.yaml \
     --atomic
 
-kubectl apply -f setup/certificate.yaml
-
 # Linkerd Dashboard
-
 helm upgrade -i linkerd-viz linkerd/linkerd-viz --namespace linkerd-viz --create-namespace
 
 # Post config
 kubectl create namespace services
-kubectl annotate namespace services "linkerd.io/inject=enabled"
+kubectl annotate namespace services "linkerd.io/inject=enabled" --overwrite
 kubectl config set-context --current --namespace=services
